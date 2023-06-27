d87d6384e33edfde98656afda5309046584ba041
yumi0109 <yumi.iwski@gmail.com>
2023-06-27 14:26:22 +0900

    task6 version3

--- task6/task6.js
+++ task6/task6.js
@@ -2,14 +2,14 @@
   'use strict';
   kintone.events.on('app.record.create.show', (event) => {
     const action5 = ['あくなき探求', '不屈の心体', '理想への共感', '心を動かす', '知識を増やす', '公明正大'];
-    const arryRow = []
+    const arryRow = [];
 
-    action5.forEach((val,elem) => {
-      arryRow[elem] = {
+    for (let i = 0; i < action5.length; i++) {
+      arryRow[i] = {
         value: {
           'Action5': {
             type: 'DROP_DOWN',
-            value: action5[elem]
+            value: action5[i]
           },
           '状況': {
             type: 'CHECK_BOX',
@@ -21,8 +21,8 @@
           }
         }
       };
-      event.record.Table.value[elem] = arryRow[elem];
-    });
+      event.record.Table.value[i] = arryRow[i];
+    };
     return event;
   });
 })();
