ba9ccab9c1913a9f76dd6a7a0a652f66739f75c7
yumi0109 <yumi.iwski@gmail.com>
2023-07-04 17:03:05 +0900

    task7 version2

--- task7/task7.js
+++ task7/task7.js
@@ -5,22 +5,31 @@
     'app.record.create.change.管理番号',
     'app.record.edit.change.日付',
     'app.record.edit.change.サイボウズ製品',
-    'app.record.edit.change.管理番号'];
+    'app.record.edit.change.管理番号',
+    'app.record.create.show',
+    'app.record.edit.show'];
 
-  kintone.events.on(['app.record.create.show', 'app.record.edit.show', field], (event) => {
+  kintone.events.on(field, (event) => {
     event.record.重複禁止項目_文字列.disabled = true;
     const day = event.record.日付.value;
     const productName = event.record.サイボウズ製品.value;
     const controlNumber = event.record.管理番号.value ? event.record.管理番号.value : '';
     let cybozuProducts = '';
-    if (productName === 'kintone') {
-      cybozuProducts = 'KN';
-    } else if (productName === 'Garoon') {
-      cybozuProducts = 'GR';
-    } else if (productName === 'サイボウズ Ofiice') {
-      cybozuProducts = 'OF';
-    } else if (productName === 'Mailwise') {
-      cybozuProducts = 'MW';
+    switch (productName) {
+      case 'kintone':
+        cybozuProducts = 'KN';
+        break;
+      case 'Garoon':
+        cybozuProducts = 'GR';
+        break;
+      case 'サイボウズ Office':
+        cybozuProducts = 'OF';
+        break;
+      case 'Mailwise':
+        cybozuProducts = 'MW';
+        break;
+      default:
+        break;
     }
     let YYYYMMDD = dateFns.format(day, 'YYYYMMDD');
     event.record.重複禁止項目_文字列.value = `${YYYYMMDD}-${cybozuProducts}-${controlNumber}`;
